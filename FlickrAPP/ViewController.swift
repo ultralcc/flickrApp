a816856647aeed2a667c0265fdfc07b16fcbea81
Ignacio Lee <ultralcc@icloud.com>
2020-05-28 09:11:40 +0800

    Initial Commit

---
+++ FlickrAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrAPP
+//
+//  Created by Ignacio Lee on 2020/5/28.
+//  Copyright © 2020 UltraLCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
